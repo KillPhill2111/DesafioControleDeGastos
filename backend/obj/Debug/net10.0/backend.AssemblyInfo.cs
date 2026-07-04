d37bc861dad27cfb22ad86005e05c6b02424d3c8
KillPhill2111 <filipe.fonseca.eng@outlook.com>
2026-07-04 14:36:11 -0300

    9º commit::fix --

--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ac918d7223a9afbfec00ba53c47745da5b673f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3df5f275ee026163c0b4035caf5ea1f0f4db3784")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
